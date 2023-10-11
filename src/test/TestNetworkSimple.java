658dfb80416804ae2647c07a5e5639d619143763
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2023-10-11 16:10:06 +0200

    add more tests

--- src/test/TestNetworkSimple.java
+++ src/test/TestNetworkSimple.java
@@ -44,11 +44,80 @@ public class TestNetworkSimple {
 		
 		petri.getListPlaces().forEach(place -> System.out.println(place.count()));
 	}
+	public static void test3() {
+		PetriNet petri = new PetriNet();
+		Place placeIn = new Place(0);
+		Place placeOut = new Place(0);
+		Transition transi = new Transition();
+		petri.add(placeIn);
+		petri.add(placeOut);
+		petri.add(transi);
+		petri.add(placeIn, transi, 2, EdgeType.ZERO);
+		petri.add(placeOut, transi, 2, EdgeType.IN);
+		
+		petri.triggerTransition(transi);
+		
+		petri.getListPlaces().forEach(place -> System.out.println(place.count()));
+	}
+	public static void test4() {
+		PetriNet petri = new PetriNet();
+		Place placeIn = new Place(1);
+		Place placeOut = new Place(0);
+		Transition transi = new Transition();
+		petri.add(placeIn);
+		petri.add(placeOut);
+		petri.add(transi);
+		petri.add(placeIn, transi, 2, EdgeType.ZERO);
+		petri.add(placeOut, transi, 2, EdgeType.IN);
+		
+		petri.triggerTransition(transi);
+		
+		petri.getListPlaces().forEach(place -> System.out.println(place.count()));
+	}
+	
+	public static void test5() {
+		PetriNet petri = new PetriNet();
+		Place placeIn = new Place(0);
+		Place placeOut = new Place(0);
+		Transition transi = new Transition();
+		petri.add(placeIn);
+		petri.add(placeOut);
+		petri.add(transi);
+		petri.add(placeIn, transi, 2, EdgeType.EMPTY);
+		petri.add(placeOut, transi, 2, EdgeType.IN);
+		
+		petri.triggerTransition(transi);
+		
+		petri.getListPlaces().forEach(place -> System.out.println(place.count()));
+	}
+	public static void test6() {
+		PetriNet petri = new PetriNet();
+		Place placeIn = new Place(5);
+		Place placeOut = new Place(0);
+		Transition transi = new Transition();
+		petri.add(placeIn);
+		petri.add(placeOut);
+		petri.add(transi);
+		petri.add(placeIn, transi, 2, EdgeType.EMPTY);
+		petri.add(placeOut, transi, 2, EdgeType.IN);
+		
+		petri.triggerTransition(transi);
+		
+		petri.getListPlaces().forEach(place -> System.out.println(place.count()));
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("test 1 :");
-		test1();
+		test1(); //Expected : 1 2 2
 		System.out.println("test 2 :");
-		test2();
+		test2(); //Expected : 1 0
+		System.out.println("test 3 :");
+		test3(); //Expected : 0 2 
+		System.out.println("test 4 :");
+		test4();// Expected : 1 0 
+		System.out.println("test 5 :");
+		test5();// Expected : 0 0 
+		System.out.println("test 6 :");
+		test6();// Expected : 0 2 
 	}
 }
