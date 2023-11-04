0e37b716fcb74cc7b37c7c3fd11a85423d47dcb5
antoinech <smb.antoine@gmail.com>
2023-11-04 15:08:56 +0100

    Slight doc modifications

--- src/petriNet/EdgeType.java
+++ src/petriNet/EdgeType.java
@@ -1,11 +1,11 @@
 package petriNet;
 
 /**
- * Enum representing the four edges types IN represents an edge directed from
+ * Enum representing the four edges types. IN represents an edge directed from
  * the transition to the place. OUT represents an edge directed from the place
  * to the transition. ZERO is a specific OUT edge, activated if the place is
- * empty EMPTY is a specitif OUT edge, actived is the place is not empty It is
- * used to describe the edge we want to build between a place and a transition
+ * empty. EMPTY is a specitif OUT edge, actived is the place is not empty It is
+ * used to describe the edge we want to build between a place and a transition.
  * 
  * @author Cheucle
  *
