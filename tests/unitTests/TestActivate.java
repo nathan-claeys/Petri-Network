85c70568b8ede258c9efad9d4c92621322193b73
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2023-11-06 17:18:08 +0100

    aled

--- tests/unitTests/TestActivate.java
+++ tests/unitTests/TestActivate.java
@@ -64,7 +64,7 @@ public class TestActivate {
         String result = String.format(
 				"RAPPORT DE L'ETAT DU RESEAU\nNombre de transitions : %d \nNombre de places : %d\n\n=====================\nEtat des places :\n",
 				1, 1);
-		result += String.format("Place %d : %d cailloux\n", 1, 0);
+		result += String.format("Place %d : %d cailloux\n", 1, 1);
 		result += "=====================\nEtat des arcs :\n";
 		// Transitions information
 		result += String.format("  Arcs de la transition %d\n", 1);
@@ -90,14 +90,14 @@ public class TestActivate {
         String result = String.format(
 				"RAPPORT DE L'ETAT DU RESEAU\nNombre de transitions : %d \nNombre de places : %d\n\n=====================\nEtat des places :\n",
 				1, 1);
-		result += String.format("Place %d : %d cailloux\n", 1, 0);
+		result += String.format("Place %d : %d cailloux\n", 1, 2);
 		result += "=====================\nEtat des arcs :\n";
 		// Transitions information
 		result += String.format("  Arcs de la transition %d\n", 1);
 
 
 		result += String.format("    Arc %d - Poids : %d , Type : %s, Cible : Place %d\n", 1,
-						2, edge.getClass().getName(), 1);
+						3, edge.getClass().getName(), 1);
 		result += "FIN DU RAPPORT\n----------------------------------------------\n";
         petriNet.triggerTransition(transition);
         assertEquals(result,petriNet.toString());
@@ -191,6 +191,25 @@ public class TestActivate {
 		result += String.format("    Arc %d - Poids : %d , Type : %s, Cible : Place %d\n", 1,
 						3, edge.getClass().getName(), 1);
 		result += "FIN DU RAPPORT\n----------------------------------------------\n";
+        petriNet.triggerTransition(transition);
+        assertEquals(result,petriNet.toString());
+    }
+
+    //RM0
+    @Test
+    void testActivateTransitionWithEdgesInAndOutAndTwoPlacesWith0And2Token (){
+        Transition transition = new Transition();
+        PetriNet petriNet = new PetriNet();
+        Place place1 = new Place(0);
+        Place place2 = new Place(2);
+        petriNet.add(transition);
+        petriNet.add(place1);
+        petriNet.add(place2);
+        petriNet.add(place1, transition, 1, EdgeType.OUT);
+        petriNet.add(place2, transition, 1, EdgeType.IN);
+        EdgeIn edgeIn = (EdgeIn) transition.getEdges().get(1);
+        EdgeOut edgeOut = (EdgeOut) transition.getEdges().get(0);
+        String result = petriNet.toString();
         petriNet.triggerTransition(transition);
         assertEquals(result,petriNet.toString());
     }
